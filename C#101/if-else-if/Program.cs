3b5ed9b8f6d46d7041aa02eae2174d269d1afb59
Murat Topallar <topallarm@outlook.com>
2023-05-01 15:05:19 +0300

    C#101 if-else-ternary eklendi.

---
+++ C#101/if-else-if/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace if_else_if
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+
+            if(time >= 6 && time < 11)
+                Console.WriteLine("Gunaydin");
+            else if (time <= 18)            
+                Console.WriteLine("Iyi gunler");
+            else
+                Console.WriteLine("Iyi geceler");
+            
+            string sonuc = time <=18 ? "Iyi Gunler" : "Iyi Geceler"; //Ternary If
+
+            sonuc = time >= 6 && time < 11 ? "Gunaydin" : time <=18 ? "Iyi Gunler" : "Iyi Geceler!"; //Ic ice ternary if *istenildigi kadar cogaltilabilir ancak cok uzadiginda okuma guclugune sebep olur.
+
+            Console.WriteLine(sonuc);
+            Console.ReadKey();
+        }
+    }
+}
