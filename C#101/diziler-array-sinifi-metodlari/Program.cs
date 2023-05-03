5edd1e7437c299e9ce390f09d8f24cf4d6f88e25
Murat Topallar <topallarm@outlook.com>
2023-05-03 22:13:30 +0300

    C#101 array sinifi metodlari eklendi.

---
+++ C#101/diziler-array-sinifi-metodlari/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace diziler_array_sinifi_metodlari
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Sort => siralama
+            int[] sayiDizisi = {23,12,4,86,72,3,11,17};
+            
+            Console.WriteLine("--------- Sirasiz Liste ---------");
+            foreach (var sayi in sayiDizisi)
+            {
+                Console.WriteLine(sayi);
+            }
+            Console.WriteLine("--------- Sirali Liste ---------");
+            Array.Sort(sayiDizisi); //void fonksiyon, dizinin kendisini direk duzenler.
+
+            foreach (var sayi in sayiDizisi)
+            {
+                Console.WriteLine(sayi);
+            }
+
+            //Clear verdigimiz dizi icerisinde, verdigimiz indexten baslayarak, verdigimiz sayida elemani sifirlamaya yarar. Silmiyor, degerine 0 degeri atiyor.
+            Console.WriteLine("--------- Array Clear ---------");
+            Array.Clear(sayiDizisi,2,2); //dizi: sayiDizisi baslangic: 2.index kacElemanIcın: 2
+            foreach (var sayi in sayiDizisi)
+            {
+                Console.WriteLine(sayi);
+            }
+
+            Console.WriteLine("--------- Array Reverse ---------");
+            //Reverse => verdigimiz diziyi ortadan itibaren ayna yansimasi gibi yer degistiriyor. Sonuc olarak arrayi ters cevirmis oluyor.
+            Array.Reverse(sayiDizisi);
+            foreach (var sayi in sayiDizisi)
+            {
+                Console.WriteLine(sayi);
+            }
+
+            //IndexOf => verdigimiz dizi icerisinde, verilen elemanin (varsa) dizideki index numarasini verir. Yoksa -1 verir.
+            Console.WriteLine("--------- Array IndexOf() ---------");
+            int indexNumarasi = Array.IndexOf(sayiDizisi,23); //sayiDizisi icinde 23 numarali eleman kacinci indexte?
+            Console.WriteLine($"Index Numarasi : {indexNumarasi}");
+
+            //Resize, diziyi yeniden bıyutlandirir.
+            Console.WriteLine("--------- Array Resize() ---------");
+            Array.Resize<int>(ref sayiDizisi,9); //<ilgili dizinin veritipi>(referans yenidenBoyutlandirilacakDİzi, yeniBoyut)
+            sayiDizisi[8] = 99;
+            foreach (var sayi in sayiDizisi)
+            {
+                Console.WriteLine(sayi);
+            }
+        }
+    }
+}
